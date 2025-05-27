95848e89d062158bf6804a7c31b2c1c901715d9c
CursedToxic <160098130+CursedToxic@users.noreply.github.com>
2025-05-28 09:36:17 +1000

    Added some functionality
    added some functionality to Subjects

--- PythonBasics/OOP/Classes/Subjects.py
+++ PythonBasics/OOP/Classes/Subjects.py
@@ -13,11 +13,14 @@ class Subject:
 
         def view_subject(new_subject_name, new_subject_year, no_subject_students):
             new_subject_name = str(subject_name)
-            new_subject_year = str(subject_year)
-            no_subject_students = print(int(subject_students))
+            new_subject_year = int(subject_year)
+            no_subject_students = int(subject_students)
 
-            return new_subject_name, new_subject_year, no_subject_students
+            print(f'There are {no_subject_students} Students in Year {new_subject_year} {new_subject_name}')
+            return new_subject_name, new_subject_year
         
         view_subject(new_subject_name=any, new_subject_year=any, no_subject_students=any)
+
+
 
         
